b1af2dcb6913a22f43ae2d5b5722ce7ec786a662
Angelina <angelinamaryan3@gmail.com>
2024-12-02 12:07:19 +0500

    change select

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,15 @@
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex items-center gap-4">
-          <select class="border rounded-md py-2 px-3 outline-none" name="" id="">
-            <option value="">По названию</option>
-            <option value="">По цене (дешевые)</option>
-            <option value="">По цене (дорогие)</option>
+          <select
+            @change="onChangeSelect"
+            class="border rounded-md py-2 px-3 outline-none"
+            name=""
+            id=""
+          >
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
@@ -18,20 +23,73 @@
             <input
               placeholder="Поиск..."
               class="border rounded-md pl-11 pr-4 py-2 outline-none focus-within:border-gray-400"
+              @change="onchangeSearch"
             />
           </div>
         </div>
       </div>
 
-      <MyCardList />
+      <div class="mt-10"><MyCardList :items="items" /></div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, ref, watch, reactive } from 'vue'
+import axios from 'axios'
+
 import MyHeader from './components/MyHeader.vue'
 import MyCardList from './components/MyCardList.vue'
-import MyDrawer from './components/MyDrawer.vue'
+
+// import MyDrawer from './components/MyDrawer.vue'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: '',
+  searchQuery: '',
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onchangeSearch = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://8b687858304794bc.mokky.dev/items')
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+})
+watch(
+  filters,
+  async () => {
+    try {
+      const { data } = await axios.get(
+        'https://8b687858304794bc.mokky.dev/items?sortBy=' + filters.sortBy,
+      )
+      items.value = data
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  filters,
+  async () => {
+    try {
+      const { data } = await axios.get(
+        'https://8b687858304794bc.mokky.dev/items?q=' + filters.searchQuery,
+      )
+      items.value = data
+    } catch (error) {
+      console.log(error)
+    }
+  },
+)
 </script>
 
 <style></style>
--- src/components/MyCardList.vue
+++ src/components/MyCardList.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
     <MyCard
-      image-url="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="12500"
-      :is-added="true"
-      :is-favorite="true"
+      v-for="item in items"
+      :key="item.id"
+      :imageUrl="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
       :onclick-add="onclickAdd"
     />
-    <MyCard image-url="/sneakers/sneakers-2.jpg" />
-    <MyCard image-url="/sneakers/sneakers-3.jpg" />
-    <MyCard image-url="/sneakers/sneakers-4.jpg" />
-    <MyCard image-url="/sneakers/sneakers-5.jpg" />
-    <MyCard image-url="/sneakers/sneakers-6.jpg" />
-    <MyCard image-url="/sneakers/sneakers-7.jpg" />
-    <MyCard image-url="/sneakers/sneakers-8.jpg" />
-    <MyCard image-url="/sneakers/sneakers-9.jpg" />
-    <MyCard image-url="/sneakers/sneakers-10.jpg" />
-    <MyCard image-url="/sneakers/sneakers-11.jpg" />
-    <MyCard image-url="/sneakers/sneakers-12.jpg" />
   </div>
 </template>
 
 <script setup>
 import MyCard from './MyCard.vue'
+defineProps({
+  items: Array,
+})
+
 const onclickAdd = () => {
-  alert('Добавлено')
+  alert('Добавлено в корзину')
 }
 </script>
